e76405d4a7580caa86b744c23bbf8cea413b07f7
sammorrisdesign <mail@sammorrisdesign.co.uk>
2018-07-02 14:56:01 -0400

    add entries

--- src/js/modules/data.js
+++ src/js/modules/data.js
@@ -11,12 +11,8 @@ module.exports =  {
     },
 
     initHandlebars: function() {
-        handlebars.registerHelper('if_eq', function(a, b, opts) {
-            if (a == b) {
-                return opts.fn(this);
-            } else {
-                return opts.inverse(this);
-            }
+        handlebars.registerHelper('handlise', function(string) {
+            return string.replace(' ', '-').toLowerCase();
         });
     },
 
