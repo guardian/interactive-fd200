edbfc77da2d21b22431ce1831bee0edc10f9ea60
Juweek Adolphe <jadolphe23@gmail.com>
2018-07-03 12:27:41 -0400

    mobile dropdown work pt 1

--- src/js/modules/scroll.js
+++ src/js/modules/scroll.js
@@ -1,5 +1,6 @@
 
-var windowTop, windowHeight, steps, section;
+var windowTop, windowHeight, steps, button;
+var sections = ['Abolitionist','Diplomat','Educator','Enterprenuer','Writer','Feminist','Politican'];
 
 module.exports =  {
     init: function() {
@@ -29,6 +30,7 @@ module.exports =  {
                 stepToShow = $(el).data('category');
             }
         }.bind(this));
+        $('.uit-mobile-nav').html('');
         this.changeNav(stepToShow);
     },
 
@@ -37,8 +39,9 @@ module.exports =  {
           $(el).removeClass('uit-highlighted');
             if ($(el).hasClass('uit-nav__category--' + step)) {
                     $(el).addClass('uit-highlighted');
+                    button = "<a href='' class='uit-share__button uit-dropdown__button'><svg class='uit-icon' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 50 50'><path d='M36.33 19.99L25 29.48l-11.33-9.49-1.11 1.11 10.23 10.22 1.63 1.63h1.16l1.63-1.63L37.44 21.1l-1.11-1.11z'/></svg></a>";
+                    $('.uit-mobile-nav').html(sections[step] + button);
                 }
        }.bind(this));
-       $('.uit-mobile-nav').html(step);
     },
 };
